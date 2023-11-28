9f6344892752f6043604da134e6b6bc83ea536ac
danmxli <danmuxingli@gmail.com>
2023-11-28 18:29:00 -0500

    bar chart visualization of accumulative commits

--- gitcomet/components/home/main-card.tsx
+++ gitcomet/components/home/main-card.tsx
@@ -1,20 +1,26 @@
 'use client'
 import React, { useState, useEffect, useRef } from "react"
 import { retrieveContributionData } from "@/modules/github-contributions"
-import CommitGraph from "./main-card/commit-graph"
 import ContributionCount from "./main-card/contribution-count"
 import { FaMailBulk, FaGithub, FaLinkedin } from "react-icons/fa";
+
+// https://github.com/recharts/recharts/issues/2272
+import dynamic from 'next/dynamic'
+const CommitGraph = dynamic(
+    () => import('./main-card/commit-graph'),
+    { ssr: false }
+)
 
 const MainCard = () => {
 
     const fetchExecuted = useRef(false)
-    const [data, setData] = useState<any>(null);
+    const [currData, setCurrData] = useState<any>(null)
     const [yearCount, setYearCount] = useState(0)
 
     const getGithubContributions = async () => {
         let user = await retrieveContributionData()
-        console.log(user)
-        setData(user)
+        console.log(user.data.user.contributionsCollection.contributionCalendar.weeks)
+        setCurrData(user.data.user.contributionsCollection.contributionCalendar.weeks)
         setYearCount(user.data.user.contributionsCollection.contributionCalendar.totalContributions)
     }
 
@@ -29,36 +35,38 @@ const MainCard = () => {
         <div className="w-full p-6 rounded-3xl bg-neutral-950 border border-neutral-700 flex flex-col gap-6 leading-relaxed">
             <div className="flex-grow max-h-fit">
                 <h1 className="text-white text-4xl font-semibold">Building Robust Solutions with Ergonomic Design.</h1>
-                <p className="text-neutral-400">I engineer innovative solutions to enhance the human experience.</p>
                 <p className="text-neutral-400">My projects aim to create scalable software services that communicate seamlessly and deliver real-time benefits to users. </p>
-                <CommitGraph />
+                {currData ? (
+                    <CommitGraph data={currData} />
+                ) : (
+                    <></>
+                )}
+
             </div>
 
             <div>
-                <h1 className="text-white text-4xl font-semibold">Contributor to Free and Open Source Software.</h1>
+                <h1 className="text-white text-4xl font-semibold">Contributor to Innovation.</h1>
                 <p className="text-neutral-400">Open to work and collaboration. Let&#39;s build something great together.</p>
-                {data ? (
-                    <div className="flex gap-6 mt-6">
+                <div className="flex gap-6 mt-6">
+                    {currData ? (
                         <div className="flex items-center">
                             <ContributionCount num={yearCount} />
                         </div>
-
-                        <div className="flex items-center justify-center border p-6 rounded-3xl border-neutral-700">
-                            <div className="">
-                                <h1 className="text-xl text-neutral-700">Contributions in the last year.</h1>
-                                <div className="mt-3 flex items-center text-2xl gap-3">
-                                    <a className="bg-neutral-400 hover:bg-white hover:cursor-pointer p-3 rounded-3xl"><FaMailBulk /></a>
-                                    <a className="bg-neutral-400 hover:bg-white hover:cursor-pointer p-3 rounded-3xl"><FaGithub /></a>
-                                    <a className="bg-neutral-400 hover:bg-white hover:cursor-pointer p-3 rounded-3xl"><FaLinkedin /></a>
-                                </div>
+                    ) : (
+                        <>
+                        </>
+                    )}
+                    <div className="flex items-center justify-center border p-6 rounded-3xl border-neutral-700">
+                        <div className="">
+                            <h1 className="text-xl text-neutral-700">Total Contributions.</h1>
+                            <div className="mt-3 flex items-center text-2xl gap-3">
+                                <a className="bg-neutral-400 hover:bg-white hover:cursor-pointer p-3 rounded-3xl"><FaMailBulk /></a>
+                                <a className="bg-neutral-400 hover:bg-white hover:cursor-pointer p-3 rounded-3xl"><FaGithub /></a>
+                                <a className="bg-neutral-400 hover:bg-white hover:cursor-pointer p-3 rounded-3xl"><FaLinkedin /></a>
                             </div>
                         </div>
                     </div>
-                ) : (
-                    <div>
-
-                    </div>
-                )}
+                </div>
             </div>
         </div>
     )
--- gitcomet/components/home/main-card/commit-graph.tsx
+++ gitcomet/components/home/main-card/commit-graph.tsx
@@ -1,9 +1,29 @@
-const CommitGraph = () => {
-    return (
-        <div>
+'use client'
+import React from 'react';
+import { ResponsiveContainer, BarChart, Bar, XAxis, YAxis, Tooltip } from 'recharts';
 
-        </div>
-    )
+interface DataProps {
+    data: any
 }
 
-export default CommitGraph
+const CommitGraph: React.FC<DataProps> = ({ data }) => {
+
+    // Flatten data
+    const result = data.flatMap((item: { contributionDays: any[]; }) => item.contributionDays.filter(day => day.contributionCount > 0));
+
+    return (
+        <div className='mt-6 h-fit'>
+            <h1 className='text-center text-white text-sm'>Accumulative commit history by year, visualized.</h1>
+            <ResponsiveContainer width="100%" height={180}>
+                <BarChart data={result} margin={{ top: 0, right: 60, bottom: 0, left: 0 }}>
+                    <XAxis dataKey="date" />
+                    <YAxis />
+                    <Tooltip />
+                    <Bar dataKey="contributionCount" fill="#16a34a" />
+                </BarChart>
+            </ResponsiveContainer>
+        </div>
+    );
+}
+
+export default CommitGraph;
