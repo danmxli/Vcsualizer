99714f08bc342a9976e4217528c7c48ad3a4b423
danmxli <danmuxingli@gmail.com>
2023-11-27 21:54:16 -0500

    sidebar ui

--- gitcomet/app/page.tsx
+++ gitcomet/app/page.tsx
@@ -1,8 +1,10 @@
 import Image from 'next/image'
+import Navbar from '@/components/navbar'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between">
+    <main className="flex min-h-screen">
+      <Navbar />
       
     </main>
   )
